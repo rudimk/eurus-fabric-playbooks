23cc4bef7af217931d4c89b269848f6552f5ccaa
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 12:57:56 +0530

    Added a function to modify the hosts file on every node, adding entries for every node.

--- ceph/prepareNodes.py
+++ ceph/prepareNodes.py
@@ -11,7 +11,7 @@ def getConnection(node):
 def setupNTP():
 	for node in inventory.CEPH_NODES:
 		conn = getConnection(node)
-		logging.info("[X] Setting up NTP on {node}".format(node=node))
+		logging.info(f"[X] Setting up NTP on {node} ==>")
 		setNTPServer = conn.run('ntpdate -s time.nist.gov')
 		updateYum = conn.run('yum update -y')
 		installNTP = conn.run('yum install -y ntp')
@@ -19,6 +19,13 @@ def setupNTP():
 		startNTPD = conn.run('systemctl start ntpd')
 	logging.info("[X] Finished setting up NTPD.")
 
+
+def addHosts():
+	for node in inventory.CEPH_NODES:
+		conn = getConnection(node)
+		logging.info(f"[X] Adding Ceph nodes to the /etc/hosts file on {node} ==>")
+		addHost = conn.run(f"cat << EOM >> /etc/hosts \n {inventory.HOST_ENTRIES} \n")
+	logging.info("[X] Added hosts.")
 
 
 
