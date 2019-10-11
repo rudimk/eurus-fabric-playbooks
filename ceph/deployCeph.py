ec7ebaf3ce6997bb52dd53cd14a1dfae7383d67d
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 19:46:20 +0530

    Added a function to install Ceph MONs on all nodes.

--- ceph/deployCeph.py
+++ ceph/deployCeph.py
@@ -8,8 +8,13 @@ def initialiseCephConfig(conn, hostnames, logging):
 
 
 def installCephBinaries(conn, hostnames, logging):
-	logging.info(f"[X] Installing Ceph binaries on all nodes ==>")
+	logging.info("[X] Installing Ceph binaries on all nodes ==>")
 	conn.local(f"cd /root/CEPH-CLUSTER && ceph-deploy install {hostnames}")
 	logging.info("Installed Ceph binaries on all nodes.")
 
+
+def installCephMon(conn, logging):
+	logging.info("[X] Installing Ceph MONs on all nodes ==>")
+	conn.local("cd /root/CEPH-CLUSTER && ceph-deploy mon create-initial")
+	logging.info("Installed Ceph MONs on all nodes.")
 
