8ab333484f5909c6a40e3b6ef66b0f24f0275aa9
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 10:40:45 +0530

    Removed the Ceph inventory file from the git tree.

--- ceph/inventory.py
+++
@@ -1,9 +0,0 @@
-# Define a list of hostnames/IP addresses that will be used for running OSDs/MONs. 
-# All nodes run Ubuntu 16.04/18.04. All commands will be run as root.
-# Also make sure the node these playbooks run on can SSH into the Ceph nodes with a SSH keypair.
-
-CEPH_NODES = ()
-
-# The path to the SSH private key that will be used
-
-SSH_KEY = ''
