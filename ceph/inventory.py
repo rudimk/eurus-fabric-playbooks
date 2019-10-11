6b042539f2c66a7df1b905d6292320b0fc7b526e
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 17:51:41 +0530

    Modified the CEPH_NODES constant to contain a list of dictionaries containing Ceph node hostnames and IPs.

--- ceph/inventory.py
+++ ceph/inventory.py
@@ -1,8 +1,10 @@
 # Define a list of hostnames/IP addresses that will be used for running OSDs/MONs. 
 # All nodes run CentOS 7. All commands will be run as root.
 # Also make sure the node these playbooks run on can SSH into the Ceph nodes with a SSH keypair.
+# Define nodes as a list of dictionaries. Example: {'hostname': 'ceph01', 'ip': '10.0.0.1'}
+# Ensure the hostname matches the hostname defined in HOST_ENTRIES.
 
-CEPH_NODES = ()
+CEPH_NODES = []
 
 # The path to the SSH private key that will be used
 
