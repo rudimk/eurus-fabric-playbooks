42a8eb266be64f0b82ae94e35077360d29902035
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 19:24:51 +0530

    Added a function to install Ceph binaries across all nodes.

--- ceph/deployCeph.py
+++ ceph/deployCeph.py
@@ -5,3 +5,11 @@ def initialiseCephConfig(conn, hostnames, logging):
 	conn.local("mkdir /root/CEPH-CLUSTER")
 	conn.local(f"cd /root/CEPH-CLUSTER && ceph-deploy new {hostnames}")
 	logging.info("Initialised Ceph cluster configuration on the local machine.")
+
+
+def installCephBinaries(conn, hostnames, logging):
+	logging.info(f"[X] Installing Ceph binaries on all nodes ==>")
+	conn.local(f"cd /root/CEPH-CLUSTER && ceph-deploy install {hostnames}")
+	logging.info("Installed Ceph binaries on all nodes.")
+
+
