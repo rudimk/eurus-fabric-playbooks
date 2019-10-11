41647c7acc0a2f8c0b4be7b26d2618b8c301d3d1
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-11 13:04:16 +0530

    Added a constant to store details of the Ceph repos on CentOS. Also added a function to create a local subprocess tunnel, and another function to add the Ceph repos on the local machine and install ceph-deploy.

--- ceph/prepareNodes.py
+++ ceph/prepareNodes.py
@@ -4,13 +4,28 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-def getConnection(node):
+CEPH_REPO = """
+
+[ceph-noarch]
+name=Ceph noarch packages
+baseurl=https://download.ceph.com/rpm-$release/el7/noarch
+enabled=1
+gpgcheck=1
+type=rpm-md
+gpgkey=https://download.ceph.com/keys/release.asc
+
+"""
+
+def getRemoteConnection(node):
 	return Connection(host=node, user='root', key_filename=inventory.SSH_KEY)
+
+def getLocalConnection():
+	return Connection(host='localhost')
 
 
 def setupNTP():
 	for node in inventory.CEPH_NODES:
-		conn = getConnection(node)
+		conn = getRemoteConnection(node)
 		logging.info(f"[X] Setting up NTP on {node} ==>")
 		setNTPServer = conn.run('ntpdate -s time.nist.gov')
 		updateYum = conn.run('yum update -y')
@@ -22,10 +37,22 @@ def setupNTP():
 
 def addHosts():
 	for node in inventory.CEPH_NODES:
-		conn = getConnection(node)
+		conn = getRemoteConnection(node)
 		logging.info(f"[X] Adding Ceph nodes to the /etc/hosts file on {node} ==>")
 		addHost = conn.run(f"cat << EOM >> /etc/hosts \n {inventory.HOST_ENTRIES} \n")
 	logging.info("[X] Added hosts.")
+
+
+def setupRepos():
+	conn = getLocalConnection()
+	logging.info("[X] Adding the Ceph repos to the local machine ==>")
+	addRepos = conn.local(f"release=mimic && cat << EOM >> /etc/yum.repos.d/ceph.repo \n {CEPH_REPO} \n")
+	updateRepos = conn.local("yum update -y")
+	installCephDeploy = conn.local("yum install ceph-deploy -y")
+	logging.info("[X] Added the Ceph repos, and installed ceph-deploy.")
+	
+
+
 
 
 
