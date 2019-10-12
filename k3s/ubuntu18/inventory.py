137ab29034d46ce78aa26e28fe506018aa93954b
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-12 18:29:03 +0530

    Updated k3s.ubuntu18.inventory to contain the HOST_ENTRIES constant.

--- k3s/ubuntu18/inventory.py
+++ k3s/ubuntu18/inventory.py
@@ -10,3 +10,11 @@ Specify the hostnames and IP address of K3s worker nodes here.
 
 K3S_WORKER_NODES = [{'hostname': '', 'ip': ''}]
 
+# Update this constant with host entries that will be populated in /etc/hosts for all nodes.
+# Use the same syntax as you would in the /etc/hosts file.
+
+HOST_ENTRIES = """
+
+
+
+"""
