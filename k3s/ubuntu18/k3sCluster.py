170ddab3cb655727753d63cee101dcd689119bf8
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-12 18:32:34 +0530

    Added a module for storing functions that'll deploy k3s. Added functions to add hostnames on the deployment node as well as k3s nodes.

---
+++ k3s/ubuntu18/k3sCluster.py
@@ -0,0 +1,13 @@
+import inventory
+
+
+def addLocalHosts(conn, logging):
+	logging.info(f"[X] Adding master and worker nodes to the /etc/hosts file on the local machine ==>")
+	addHost = conn.run(f"cat << EOM >> /etc/hosts \n {inventory.HOST_ENTRIES} \n")
+	logging.info("[X] Added hosts to the local machine.")
+
+
+def addRemoteHosts(hostname, conn, logging):
+	logging.info(f"[X] Adding master and worker nodes to the /etc/hosts file on {hostname} ==>")
+	addHost = conn.run(f"cat << EOM >> /etc/hosts \n {inventory.HOST_ENTRIES} \n")
+	logging.info(f"[X] Added hosts to the /etc/hosts file on {hostname}.")
