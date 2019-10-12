9ad5300da8374396adc0a7a5c37927417380b969
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-12 18:24:42 +0530

    Added a bare inventory file, to store hostnames and IP addresses for K3s master and worker nodes.

---
+++ k3s/ubuntu18/inventory.py
@@ -0,0 +1,12 @@
+'''
+Specify the hostname and IP address of the K3s master node here.
+'''
+
+K3S_MASTER_NODE = {'hostname': '', 'ip': ''}
+
+'''
+Specify the hostnames and IP address of K3s worker nodes here.
+'''
+
+K3S_WORKER_NODES = [{'hostname': '', 'ip': ''}]
+
