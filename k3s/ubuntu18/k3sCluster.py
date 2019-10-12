2da12c3847554371c31732746add78b44745a2e8
Rudraksh MK <rudimk.cloud@gmail.com>
2019-10-12 18:54:51 +0530

    Added a function to update apt packages on all k3s nodes.

--- k3s/ubuntu18/k3sCluster.py
+++ k3s/ubuntu18/k3sCluster.py
@@ -11,3 +11,9 @@ def addRemoteHosts(hostname, conn, logging):
 	logging.info(f"[X] Adding master and worker nodes to the /etc/hosts file on {hostname} ==>")
 	addHost = conn.run(f"cat << EOM >> /etc/hosts \n {inventory.HOST_ENTRIES} \n")
 	logging.info(f"[X] Added hosts to the /etc/hosts file on {hostname}.")
+
+
+def updatePackages(hostname, conn, logging):
+	logging.info(f"[X] Updating apt packages on {hostname} ==>")
+	conn.run("apt update")
+	logging.info(f"Updated apt packages on {hostname}.")
--- k3s/ubuntu18/main.py
+++ k3s/ubuntu18/main.py
@@ -15,6 +15,8 @@ def getLocalConnection():
 if __name__ == '__main__':
 	logging.info("[X] Initialising K3s deployment ==>")
 	k3sCluster.addLocalHosts(getLocalConnection(), logging)
-	k3sCluster.addRemoteHosts(getRemoteConnection(inventory.K3S_MASTER_NODE['hostname']))
+	k3sCluster.addRemoteHosts(inventory.K3S_MASTER_NODE['hostname'], getRemoteConnection(inventory.K3S_MASTER_NODE['hostname']), logging)
+	k3sCluster.updatePackages(inventory.K3S_MASTER_NODE['hostname'], getRemoteConnection(inventory.K3S_MASTER_NODE['hostname']), logging)
 	for node in inventory.K3S_WORKER_NODES:
-		k3sCluster.addRemoteHosts(getRemoteConnection(node['hostname']))
+		k3sCluster.addRemoteHosts(node['hostname'], getRemoteConnection(node['hostname']), logging)
+		k3sCluster.updatePackages(node['hostname'], getRemoteConnection(node['hostname']), logging)
